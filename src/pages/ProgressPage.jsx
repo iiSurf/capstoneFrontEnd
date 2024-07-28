cd5d3d7971f0f0ccdc37064ca20199a01c8d81bc
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-28 13:20:31 -0700

    added ProgressPage jsx

--- src/pages/ProgressPage.jsx
+++ src/pages/ProgressPage.jsx
@@ -1,1 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ProgressPage = () => {
+  const [progress, setProgress] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/front/progress")
+      .then((response) => {
+        setProgress(response.data);
+      })
+      .catch((err) => {
+        console.error("Error with fetching", err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Your Progress Journey</h1>
+      <ul>
+        {progress.map((item) => (
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ProgressPage;
