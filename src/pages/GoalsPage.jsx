aba1bf7b3325ece4f5753713ed0b760609a7226d
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-29 11:21:37 -0700

    populated goals page

--- src/pages/GoalsPage.jsx
+++ src/pages/GoalsPage.jsx
@@ -1,30 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 
 const GoalsPage = () => {
-  const [goals, setGoals] = useState([]);
+    const [goals, setGoals] = useState([]);
+    const [newGoal, setNewGoal] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("/front/goals")
-      .then((response) => {
-        setGoals(response.data);
-      })
-      .catch((err) => {
-        console.error("Error with fetching", err);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get("/front/goals")
+            .then((response) => {
+                setGoals(response.data);
+        })
+        .catch((err) => {
+            console.error('Error with fetching the goals', err);
+        });
+    }, []);
 
-  return (
-    <>
-      <h1>Your Goals Start Here</h1>
-      <ul>
-        {goals.map((item) => {
-          <li key={item.id}>{item.description}</li>;
-        })}
-      </ul>
-    </>
-  );
+    const handleAddGoal = () => {
+        axios
+            .post("/front/goals", { goal: newGoal })
+            .then((response) => {
+                setGoals([...goals, response.data]);
+                setNewGoal("");
+            })
+            .catch((err) => {
+                console.error('Error with adding a new goal', err)
+            })
+    };
+
+    return (
+        <div>
+            <h1>Your Goals</h1>
+            <ul>
+                {goals.map((goal) => {
+                    <li key={goal._id}>{goal.goal}</li>
+                })}
+            </ul>
+            <input
+                type="text"
+                value={newGoal}
+                onChange={(e) => setNewGoal(e.target.value)}
+                placeholder="Add your goals here"
+                />
+                <button onClick={handleAddGoal}>Add a Goal</button>
+        </div>
+    );
+
 };
 
 export default GoalsPage;
