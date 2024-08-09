44576d64300fb0e7184c18b3a65e9945bd05b7e9
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-08-09 16:14:28 -0700

    fixed navbar css styling import error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import placeholderLogo from '../assets/placeholderLogo.svg';
+import '../styles/NavBar.css';
 
 const NavBar = () => {
   return (
     <>
-      <div className="nabar">
+      <div className="navbar">
         <div className="navbar-container"></div>
-        
+          <Link to="/" className="navbar-logo">
+            NICK <img src={placeholderLogo} alt="logo" className="logo" />
+          </Link>
+          <div className="menu-icon"></div>
       </div>
     </>
   );
