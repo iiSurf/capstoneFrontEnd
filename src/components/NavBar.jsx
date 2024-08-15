6fabe67019859de2d19900717866252a0d1e9c9d
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-08-15 14:29:26 -0700

    added navigational links structure to NavBar jsx mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ import '../styles/NavBar.css';
 
 const NavBar = () => {
   const [click, setClick] = useState(false);
+  const closeMobileMenu = () => setClick(false);
 
   const handleClick = () => setClick(!click);
   return (
@@ -20,7 +21,28 @@ const NavBar = () => {
           <div className='menu-icon' onClick={handleClick}>
             <FontAwesomeIcon icon={ click ? faTimes : faBars } size='lg' />
           </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu} />
+              Home
+            </li>
+            <li className='nav-item'>
+              <Link to='/progress' className='nav-links' onClick={closeMobileMenu} />
+              Progress
+            </li>
+            <li className='nav-item'>
+              <Link to='/goals' className='nav-links' onClick={closeMobileMenu} />
+              Goals
+            </li>
+            <li className='nav-item'>
+              <Link to='/videos' className='nav-links' onClick={closeMobileMenu} />
+              Videos
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu} />
+              Sihn Up
+            </li>
+          </ul>
       </div>
     </>
   );
