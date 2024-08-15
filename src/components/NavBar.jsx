ee8627ee33af2a7fccb88aa7586afbdfaa13a37d
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-08-15 16:21:22 -0700

    adding button and button styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
-import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import placeholderLogo from '../assets/placeholderLogo.svg';
-import '../styles/NavBar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import placeholderLogo from "../assets/placeholderLogo.svg";
+import "../styles/NavBar.css";
 
 const NavBar = () => {
   const [click, setClick] = useState(false);
@@ -14,35 +14,47 @@ const NavBar = () => {
     <>
       <div className="navbar">
         <div className="navbar-container"></div>
-          <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo">
           <img src={placeholderLogo} alt="logo" className="logo" />
           Nick
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <FontAwesomeIcon icon={ click ? faTimes : faBars } size='lg' />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu} />
-              Home
-            </li>
-            <li className='nav-item'>
-              <Link to='/progress' className='nav-links' onClick={closeMobileMenu} />
-              Progress
-            </li>
-            <li className='nav-item'>
-              <Link to='/goals' className='nav-links' onClick={closeMobileMenu} />
-              Goals
-            </li>
-            <li className='nav-item'>
-              <Link to='/videos' className='nav-links' onClick={closeMobileMenu} />
-              Videos
-            </li>
-            <li className='nav-item'>
-              <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu} />
-              Sihn Up
-            </li>
-          </ul>
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <FontAwesomeIcon icon={click ? faTimes : faBars} size="lg" />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu} />
+            Home
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/progress"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            />
+            Progress
+          </li>
+          <li className="nav-item">
+            <Link to="/goals" className="nav-links" onClick={closeMobileMenu} />
+            Goals
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/videos"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            />
+            Videos
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            />
+            Sihn Up
+          </li>
+        </ul>
       </div>
     </>
   );
