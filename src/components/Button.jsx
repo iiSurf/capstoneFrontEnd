fba81c2a2f32b2db3546bfd6eb05919dabb269b9
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-08-16 13:25:36 -0700

    added button css styling and finished creating the button for the nav bar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "../styles/Button.css";
 import { Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn--outline'];
-const SIZES = ['btn--medium', 'btn--large'];
+const STYLES = ["btn--primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
   children,
@@ -12,15 +12,21 @@ export const Button = ({
   buttonStyle,
   buttonSize,
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    return (
-        <Link to='sign-up' className='btn-mobile'>
-            <button>
-                {children}
-            </button>
-        </Link>
-    )
+  return (
+    <Link to="sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 };
