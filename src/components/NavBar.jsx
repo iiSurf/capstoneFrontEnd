6fa7cfc8ead9eeca6aa8ecb8ac9970f37de7df89
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-08-16 15:14:21 -0700

    added navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,55 +7,74 @@ import "../styles/NavBar.css";
 
 const NavBar = () => {
   const [click, setClick] = useState(false);
-  const closeMobileMenu = () => setClick(false);
+  const [button, setButton] = useState(true);
 
+  const closeMobileMenu = () => setClick(false);
   const handleClick = () => setClick(!click);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-container"></div>
-        <Link to="/" className="navbar-logo">
-          <img src={placeholderLogo} alt="logo" className="logo" />
-          Nick
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <FontAwesomeIcon icon={click ? faTimes : faBars} size="lg" />
+      <nav>
+        <div className="navbar">
+          <div className="navbar-container"></div>
+          <Link to="/" className="navbar-logo">
+            <img src={placeholderLogo} alt="logo" />
+            Nick
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <FontAwesomeIcon icon={click ? faTimes : faBars} size="lg" />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu} />
+              Home
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/progress"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              />
+              Progress
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/goals"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              />
+              Goals
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/videos"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              />
+              Videos
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              />
+              Sihn Up
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu} />
-            Home
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/progress"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            />
-            Progress
-          </li>
-          <li className="nav-item">
-            <Link to="/goals" className="nav-links" onClick={closeMobileMenu} />
-            Goals
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/videos"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            />
-            Videos
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/sign-up"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            />
-            Sihn Up
-          </li>
-        </ul>
-      </div>
+      </nav>
     </>
   );
 };
