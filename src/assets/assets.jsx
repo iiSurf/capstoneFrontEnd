908969bebed0b9c144550abd1f12518c4a0a700b
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-09-14 18:14:24 -0700

    added images and product details

---
+++ src/assets/assets.jsx
@@ -0,0 +1,44 @@
+import image1 from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
+import image7 from './assets/image7.jpg';
+import image8 from './assets/image8.jpg';
+import image9 from './assets/image9.jpg';
+import image10 from './assets/image10.jpg';
+import image11 from './assets/image11.jpg';
+import image12 from './assets/image12.jpg';
+import image13 from './assets/image13.jpg';
+import image14 from './assets/image14.jpg';
+import image15 from './assets/image15.jpg';
+import image16 from './assets/image16.jpg';
+import image17 from './assets/image17.jpg';
+import image18 from './assets/image18.jpg';
+import image19 from './assets/image19.jpg';
+import image20 from './assets/image20.jpg';
+import image21 from './assets/image21.jpg';
+import image22 from './assets/image22.jpg';
+import image23 from './assets/image23.jpg';
+import image24 from './assets/image24.jpg';
+import image25 from './assets/image25.jpg';
+import image26 from './assets/image26.jpg';
+
+export const placeholder = [
+    {
+        _id: 'aaaa',
+        name: 'image1',
+        description: 'crowd',
+        price: 200,
+        image: [image1],
+        category: 'concert',
+        subCategory: 'people',
+        sizes: ['S', 'M', 'L'],
+        date: '1234567891023',
+        bestseller: true
+    },
+    {
+        _id: 'aaaab',
+    }
+]
