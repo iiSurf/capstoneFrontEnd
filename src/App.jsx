5294003cb29ec2236c85e02da95005701ee8037e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-10-14 22:47:17 -0700

    The homepage is working and has a headline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Game from './pages/Game';
-import BoxBreathing from './pages/BoxBreathing';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Login from './pages/Login';
+// import Game from './pages/Game';
+// import BoxBreathing from './pages/BoxBreathing';
 
 function App() {
 
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/game" element={<Game />} />
-        <Route path="/box-breathing" element={<BoxBreathing />} />
+        <Route path="/box-breathing" element={<BoxBreathing />} /> */}
       </Routes>
     </Router>
   )
