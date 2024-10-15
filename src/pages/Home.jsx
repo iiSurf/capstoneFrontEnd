8602893e81ad56bf7a7c28a8e43720557084c9b3
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-10-14 22:24:03 -0700

    added homepage content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center h-screen bg-blue-100">
+            <h1 className="text-4x1 font-bold text-center">Welcome to the Mental Health Application</h1>
+        </div>
+    );
+};
+
+export default Home;
