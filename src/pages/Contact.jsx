0f345101ad1177d9149b35b5756a0f1fe553ffa1
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-11-04 17:46:11 -0800

    added more buttons to home page. Currently trying to figure out tailwind animations on home page and figure out why it is not working. Could also be it is working and I dont see it.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,10 @@
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+    <h1 className="text-4x1 font-bold text-center">
+      Welcome to the Contact Page
+    </h1>
+  </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,44 @@
 import { Link } from "react-router-dom";
+import '../styles/Home.css';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-blue-200 to-indigo-300 text-center p-8">
-      <h1 className="text-4x1 md:text-6x1 font-bold text-gray-800 mb-4">
+      <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4 fade-in">
         Welcome to the Mental Health Application. Your Wellness Space.
       </h1>
-      <p className="text-lg md:text-x1 text-gray-600 mb-8">
+      <p className="text-lg md:text-xl text-gray-600 mb-8 fade-in">
         Discover tools and resources to help you unwind and reconnect with
         yourself.
       </p>
-      <div className="space-x-4">
+      <div className="space-x-4 fade-in">
+      <Link
+          to="/login"
+          className="px-6 py-3 bg-yellow-500 text-white rounded-lg shadow-md hover:bg-yellow-600 fade-in"
+        >
+          Login
+        </Link>
         <Link
           to="/about"
-          className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+          className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 fade-in"
         >
           About
         </Link>
         <Link
           to="/contact"
-          className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600"
+          className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 fade-in"
         >
           Contact
         </Link>
         <Link
+          to="/box-breathing"
+          className="px-6 py-3 bg-orange-500 text-white rounded-lg shadow-md hover:bg-orange-600 fade-in"
+        >
+          Box Breathing
+        </Link>
+        <Link
           to="/game"
-          className="px-6 py-3 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600"
+          className="px-6 py-3 bg-purple-500 text-white rounded-lg shadow-md hover:bg-purple-600 fade-in"
         >
           Relaxation Game
         </Link>
